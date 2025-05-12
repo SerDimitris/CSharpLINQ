f3cb4f7c17869ce571a62fd0d0660ef455335eed
Dimitris Serafeimidis <135410825+SerDimitris@users.noreply.github.com>
2025-05-12 12:31:15 +0300

    new branch commit

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Write a program in C# Sharp to remove items from
+ * list using remove function by passing the object.
+ * 
+ * Test Data :
+ * Here is the list of items :
+ * Char: m
+ * Char: n
+ * Char: o
+ * Char: p
+ * Char: q
+ * 
+ * Expected Output:
+ * Here is the list after removing the item 'o' from the list :
+ * Char: m
+ * Char: n
+ * Char: p
+ * Char: q
+ */
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ItemManager itemManager = new ItemManager();
+
+            while (true)
+            {
+                Console.WriteLine("Please insert a char to store. Press 0 to exit.");
+
+                if (Char.TryParse(Console.ReadLine(), out char result))
+                {
+                    if (result == '0') break;
+                    itemManager.AddChar(result);
+                } else Console.WriteLine("Invalid input, try again.");
+            }
+
+            Console.WriteLine("Insert a char to remove.");
+            Char.TryParse(Console.ReadLine(), out char charToDelete);
+            itemManager.RemoveChar(charToDelete);
+            itemManager.PrintChar();
+           
+        }
+
+        public class ItemManager()
+        {
+            List<Char> characters = new List<Char>();
+
+            public void AddChar(Char ch)
+            {
+                characters.Add(ch);
+            }
+
+            public bool RemoveChar(Char ch)
+            {
+                return characters.Remove(ch);
+            }
+
+            public void PrintChar()
+            {
+                Console.WriteLine("The updated list is:");
+                foreach (char ch in characters) Console.WriteLine(ch);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Write a program in C# Sharp to remove items from
+ * list by creating an object internally by filtering.
+ * 
+ * Test Data :
+ * Here is the list of items :
+ * Char: m
+ * Char: n
+ * Char: o
+ * Char: p
+ * Char: q
+ * 
+ * Expected Output :
+ * Here is the list after removing the item 'p' from the list :
+ * Char: m
+ * Char: n
+ * Char: o
+ * Char: q
+ */
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> list = new List<char>();
+            list.Add('m');
+            list.Add('o');
+            list.Add('p');
+            list.Add('q');
+
+            Console.WriteLine("The original list.");
+            foreach (char c in list) Console.WriteLine(c);
+
+            char toRemove = 'p';
+
+            var filtered = list.Where(ch => ch != toRemove);
+
+            Console.WriteLine("The updated list.");
+            foreach (char ch in filtered) Console.WriteLine(ch);
+        }
+    }
+}
